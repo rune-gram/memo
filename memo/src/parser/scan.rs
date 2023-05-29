5cd1b941925a10a8a7757349c24d0c734f60a853
zonblade <agungzonblade@gmail.com>
2023-05-29 18:19:10 +0700

    fix variables values

--- memo/src/parser/scan.rs
+++ memo/src/parser/scan.rs
@@ -1,26 +1,30 @@
 use super::token::Tokens;
 use logos::Logos;
 
+#[derive(Debug, Clone)]
+pub struct Tval {
+    token: Tokens,
+    value: String,
+}
+
 pub struct Scans {
     pub content: String,
 }
 
 impl Scans {
     pub fn new(content: String) -> Scans {
-        Scans {
-            content: content,
-        }
+        Scans { content: content }
     }
-
-    fn seek_next_token(){}
 
     pub fn start(&self) {
         let mut lex = Tokens::lexer(&self.content);
-        let mut two_d_vector  = Vec::new();
+        let mut two_d_vector = Vec::new();
         let mut vector_inside = Vec::new();
         loop {
             let token = lex.next();
-            if token == None {break;}
+            if token == None {
+                break;
+            }
             match token {
                 Some(data) => {
                     match data {
@@ -32,24 +36,60 @@ impl Scans {
                                     two_d_vector.push(vector_inside);
                                     vector_inside = Vec::new();
                                     continue;
-                                },
+                                }
                                 _ => {
-                                    vector_inside.push(vec![data.to_string(), lex.slice().to_string()]);
+                                    let vec_val = Tval {
+                                        token: data,
+                                        value: lex.slice().to_string(),
+                                    };
+                                    vector_inside.push(vec_val);
                                 }
                             }
-                        },
+                        }
                         _ => {}
                     }
-                },
+                }
                 _ => {}
             }
         }
-        // destroying vector_inside
-        vector_inside = Vec::new();
-        
+        let mut final_d_vector = Vec::new();
         // processing the variable
-        
+        // to combine multiple things into one.
+        for vector in two_d_vector {
+            let vtok_length = vector.len() - 1;
+            let mut cloned_vec = vector.clone();
+            let mut vtok_var = Vec::new();
+            let mut var_position_first = 0usize;
+            for (i, vtok) in vector.iter().enumerate() {
+                // seek check next does it have a variable?
+                let next_index = {
+                    if i == vtok_length {
+                        vtok_length
+                    } else {
+                        i + 1
+                    }
+                };
+                if vtok.token == Tokens::Variable {
+                    if vtok.token == Tokens::Variable {
+                        if var_position_first == 0 {
+                            var_position_first = i;
+                        } else {
+                            cloned_vec.remove(var_position_first+1);
+                        }
+                    }
+                    vtok_var.push(vtok.value.clone());
+                }
+                if vector[next_index].token != Tokens::Variable {
+                    break;
+                }
+            }
+            // replace value on index
+            cloned_vec[var_position_first].value = vtok_var.join("");
+            // bulk remove from vec
+            println!("\n\n");
+            final_d_vector.push(cloned_vec);
+        }
 
-        println!("{:?}", two_d_vector);
+        println!("{:?}", final_d_vector);
     }
 }
